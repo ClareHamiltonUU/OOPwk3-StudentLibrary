e42a86ebf0e22a8e8742601d417794f07a7425a5
clare <clare@DESKTOP-OHBTNTC>
2024-10-21 16:13:25 +0100

    Add project files.

---
+++ Practical3/Program.cs
@@ -0,0 +1,24 @@
+﻿// B00882634 Clare Hamilton
+using StudentLibrary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instance using default constructor
+        Student student1 = new Student();
+
+        //instance using custom constructor
+        Student student2 = new Student();
+
+        student1.Display();
+        student2.Display();
+
+        student1.GetOlder();
+        student2.GetOlder();
+         
+        Console.WriteLine("Implementing Student Older");
+        student1.Display();
+        student2.Display();
+    }
+
+}
---
+++ Practical3/Student.cs
@@ -0,0 +1,65 @@
+﻿// B00882634 Clare Hamilton
+
+namespace StudentLibrary
+{
+    internal class Student
+    {
+        //private fields
+        private int id;
+        private string name;
+        private int age;
+        private static int studentCount = 0;
+
+        //public properties
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age {
+            get { return age; }
+            set { age = value; }
+        }
+
+        //public static property - read only
+        public static int StudentCount
+        {
+            get { return studentCount; }
+        }
+
+
+        //default constructor
+        public Student()
+        {
+            name = "John Doe";
+            age = 16;
+            id = studentCount++;
+        }
+
+        //custom constructor
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            id = studentCount++;
+        }
+
+        //Display Method
+        public void Display()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}, Age: {Age}");
+        }
+
+        //GetOlder Method
+        public int GetOlder()
+        {
+            return age++;
+        }
+    }
+}
